0046587892ba0477ce4b953cbca18c157e0dca9b
aungthuhein2005 <aungthuhein.ath05@gmail.com>
2026-07-14 22:43:14 +0700

    fix preview design

--- src/markdowneditor/controller/PreviewController.java
+++ src/markdowneditor/controller/PreviewController.java
@@ -4,6 +4,8 @@
  */
 package markdowneditor.controller;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import javax.swing.JEditorPane;
 import javax.swing.JTextArea;
@@ -12,36 +14,62 @@ import java.util.Arrays;
 import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
+import java.nio.charset.StandardCharsets;
 
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.embed.swing.JFXPanel;
 /**
  *
  * @author Aung Thu Hein
  */
 public class PreviewController {
-    
+
     private final List<Extension> extensions = Arrays.asList(TablesExtension.create());
     private final Parser parser = Parser.builder().extensions(extensions).build();
     private final HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
-    
-    
-    private final JEditorPane previewPane;
+
+    private final JFXPanel previewPane;
     private final JTextArea sourceArea;
-    
-    public PreviewController(JEditorPane previewPane, JTextArea sourceArea){
+    private WebView webView;
+    public PreviewController(JFXPanel previewPane, JTextArea sourceArea) {
         this.previewPane = previewPane;
         this.sourceArea = sourceArea;
-    }
+        
+        Platform.runLater(() -> {
+
+            webView = new WebView();
     
-    public void updatePreview(){
+            Scene scene = new Scene(webView);
+    
+            previewPane.setScene(scene);
+    
+        });
+    }
+
+    public void updatePreview() {
         String markdown = sourceArea.getText();
         String bodyHtml = renderer.render(parser.parse(markdown));
-        String styledHtml = "<html><head><style>"
-            + "body { font-family: sans-serif; font-size: 10pt; }"
-            + "table { border-collapse: collapse; margin: 8px 0; }"
-            + "table, th, td { border: 1px solid #999; }"
-            + "th, td { padding: 4px 8px; }"
-            + "</style></head><body>" + bodyHtml + "</body></html>";
-        previewPane.setText(styledHtml);
+        String css = loadCss();
+        String html
+                = "<html><head><style>"
+                + css
+                + "</style></head><body>"
+                + bodyHtml
+                + "</body></html>";
+                Platform.runLater(() -> {
+                    webView.getEngine().loadContent(html);
+                });
     }
-    
+
+    private String loadCss() {
+        try (InputStream in = getClass().getResourceAsStream("/css/style.css")) {
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 }
